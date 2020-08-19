0bdc5c74f88d22729fa1772bc2e71b931b031bb2
Lucas <lucas.andre22@yahoo.com>
2020-08-18 21:01:33 -0300

    Added palindrome check

--- palindrome.c
+++ palindrome.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+char *unlimited_scanf(char *string)
+{
+    char letter;
+    int i;
+    string=(char *)malloc(sizeof(char));
+    if(!string)
+        return NULL;
+    scanf("%c", &letter);
+    for(i=0;letter!='\n';i++)
+    {
+        string=(char *)realloc(string,(i+2)*sizeof(char));
+        string[i]=letter;
+        scanf("%c", &letter);
+    }
+    string[i]='\0';
+    return string;
+}
+
 int main()
 {
-    int tamanho, tamanho_sem_espacos = 0, palindroma = 1;
-    char* vetor;
-    scanf("%d", &tamanho);
-    getchar();
-    vetor = (char*)malloc(tamanho*sizeof(char));
-    fgets(vetor,tamanho+2,stdin);
-    vetor[strlen(vetor)-1] = '\0';
-    for(int i = 0; i < tamanho; i++)
-    {
-        if(vetor[i] == ' ')
-        {
-            for(int j = i; j < tamanho-tamanho_sem_espacos; j++)
-                vetor[j] = vetor[j+1];
-            tamanho--;
+    int size, size_without_space = 0, palindroma = 1;
+    char* string;
+    printf("Palindrome check\nPlease write your phrase : ");
+    string = unlimited_scanf(string);
+    size = strlen(string);
+    for(int i = 0; i < size; i++) {
+        if(string[i] == ' ') {
+            for(int j = i; j < size-size_without_space; j++)
+                string[j] = string[j+1];
+            size--;
         }
     }
-    //printf("Frase sem espacos: \n%s\n", vetor);
-    for(int i = 0; i < tamanho-1; i++)
-    {
-        if(vetor[i] != vetor[(tamanho-1)-i]) {
-            printf("Nao eh palindroma!");
-            return 0;
+    for(int i = 0; i < size-1; i++) {
+        if(string[i] != string[(size-1)-i]) {
+            printf("It's not a palindrome!");
+            palindroma = 0;
+            break;
         }
     }
-    printf("Eh palindroma!");
+    if(palindroma)
+        printf("It's a palindrome!");
+    free(string);
     return 0;
 }
