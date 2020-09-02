32f2c90f2c7f3c3699bf15985d969666890cc117
Lucas <lucas.andre22@yahoo.com>
2020-09-02 02:18:07 -0300

    Template linked list using c++

---
+++ TemplateLinkedList.h
@@ -0,0 +1,78 @@
+#pragma once
+
+//It was made by git.com/lucasandre22
+
+namespace List {
+	template <class T>
+	class List {
+	private:
+		template <class T1>
+		class Node {
+		private:
+			T1 value;
+			Node<T1>* next;
+			Node<T1>* previous;
+		public:
+			Node(T1 value) { this->value = value; this->next = nullptr; this->previous = nullptr; }
+			~Node() { }
+			T1 GetValue() const { return this->value; }
+			Node<T1>* GetNext() const { return this->next; }
+			void SetNext(Node<T>* next) { this->next = next; }
+			Node<T1>* GetPrevious() const { return this->previous; }
+			void SetPrevious(Node<T>* previous) { this->previous = previous; }
+		};
+		Node<T>* head;
+		int size;
+	public:
+		template <class T2> friend class Iterator;
+		List() { this->head = nullptr; this->size = 0; }
+		~List() { Delete(); }
+		void Delete() { for (Node<T>* aux = this->head; aux != nullptr; aux = this->head) { this->head = aux->GetNext(); delete aux; } }
+		void Include(T value);
+		void Remove(T value);
+		int GetSize() const { return this->size; }
+		Node<T>* GetHead() const { return this->head; }
+	};
+
+	template <class T2>
+	class Iterator {
+	private:
+		List<T2>::Node<T2>* iterator;
+	public:
+		Iterator() { iterator = nullptr; }
+		~Iterator() { }
+		void SetIteratorHead(List<T2>& list) { this->iterator = list.GetHead(); }
+		T2 GetIterator() const { return this->iterator->GetValue(); }
+		bool IteratorNull() const { return this->iterator == nullptr ? true : false; }
+		void operator++() { this->iterator = this->iterator->next; }
+		void operator=(List<T2>& list) { SetIteratorHead(list); }
+	};
+
+	template <class T>
+	void List<T>::Include(T value)
+	{
+		Node<T>* aux = new Node<T>(value);
+		aux->SetNext(this->head);
+		if (this->head)
+			this->head->SetPrevious(aux);
+		this->head = aux;
+		this->size++;
+	}
+
+	template <class T>
+	void List<T>::Remove(T value)
+	{
+		Node<T>* aux = this->head;
+		for(; aux!=nullptr && aux->GetValue() != value; aux = aux->GetNext()) {}
+		if (aux == nullptr)
+			return;
+		if (aux->GetNext() != nullptr)
+			aux->GetNext()->SetPrevious(aux->previous);
+		if (aux->GetPrevious() != nullptr)
+			aux->GetPrevious->SetNext(aux->next);
+		if (this->head == aux)
+			this->head = aux->GetNext();
+		this->size--;
+		delete* aux;
+	}
+}
